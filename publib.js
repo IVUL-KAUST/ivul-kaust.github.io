1f1f75bb49a7388539e598185ee0c9dc3c4a349e
Juan C. Pérez <jc.perez13@uniandes.edu.co>
2021-12-16 10:39:29 +0300

    Add BAOD

--- publib.js
+++ publib.js
@@ -7,6 +7,11 @@ const publib = {
     'link': ['Code', 'Data', 'Video', 'Poster', 'Slides', 'More', 'Website'],
     'keys': ['handle', 'theme', 'year', 'venue', 'thumbnail', 'paper', 'title', 'authors', 'coauthors', 'distinctions', 'links', 'abstract'],
     'lib': [
+	    ['10754/670906', 2, 21, 3, '1MdqlD7CUn5hMJVqzNlwgiNNHch3Xh12J', '14D4pOKMniqMWli391fi1NTok_D_JUa4C', 'BAOD: Budget-Aware Object Detection', [99, 10, 1, 96, 0],
+            [1, 1, 0, 0, 0],
+            [3],
+            ['', 0, 'https://www.youtube.com/watch?v=khJOvrZhL14', '', '', '', 'https://www.alejandropardo.net/publication/baod/'], "We study the problem of object detection from a novel perspective in which annotation budget constraints are taken into consideration, appropriately coined Budget Aware Object Detection (BAOD). When provided with a fixed budget, we propose a strategy for building a diverse and informative dataset that can be used to optimally train a robust detector. We investigate both optimization and learning-based methods to sample which images to annotate and what type of annotation (strongly or weakly supervised) to annotate them with. We adopt a hybrid supervised learning framework to train the object detector from both these types of annotation. We conduct a comprehensive empirical study showing that a handcrafted optimization method outperforms other selection techniques including random sampling, uncertainty sampling and active learning. By combining an optimal image/annotation selection scheme with hybrid supervised learning to solve the BAOD problem, we show that one can achieve the performance of a strongly supervised detector on PASCAL-VOC 2007 while saving 12.8% of its original annotation budget. Furthermore, when 100% of the budget is used, it surpasses this performance by 2.0 mAP percentage points."
+        ],
 	    ['10754/660731', 2, 20, 2, '1y8VPQ2nlrHeY9JtjIYkUTDgBB1rULMVX', '1aDvx5ro3g0kAYoEjxXjjV183NYBgjSbl', 'SGAS: Sequential Greedy Architecture Search', [4, 130, 132, 21, 1, 0],
             [1, 1, 1, 0, 0, 0],
             [],
