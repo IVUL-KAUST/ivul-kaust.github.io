683c2873d7405163ae982a84a985b4cb1eccaf40
Juan C. Pérez <jc.perez13@uniandes.edu.co>
2022-04-28 10:14:29 +0300

    Include MAD

--- publib.js
+++ publib.js
@@ -7,6 +7,11 @@ const publib = {
     'link': ['Code', 'Data', 'Video', 'Poster', 'Slides', 'More', 'Website'],
     'keys': ['handle', 'theme', 'year', 'venue', 'thumbnail', 'paper', 'title', 'authors', 'coauthors', 'distinctions', 'links', 'abstract'],
     'lib': [
+	     ['10754/673930', 1, 22, 2, '1OWVtUM3YYXq2e3pBLszwFpUcIQP0FvgX', '152AtWrr6FxX6m1sCPhQ5A3o5-m6zPsy1', 'MAD: A Scalable Dataset for Language Grounding in Videos from Movie Audio Descriptions', [151, 99, 127, 14, 98, 2, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [],
+            ['https://github.com/Soldelli/MAD', 0, 'https://webcast.kaust.edu.sa/Mediasite/Showcase/default/Presentation/88b31db635ba445bb5fcbf43f7d5136f1d', '', '', '', ''], "The recent and increasing interest in video-language research has driven the development of large-scale datasets that enable data-intensive machine learning techniques. In comparison, limited effort has been made at assessing the fitness of these datasets for the video-language grounding task. Recent works have begun to discover significant limitations in these datasets, suggesting that state-of-the-art techniques commonly overfit to hidden dataset biases. In this work, we present MAD (Movie Audio Descriptions), a novel benchmark that departs from the paradigm of augmenting existing video datasets with text annotations and focuses on crawling and aligning available audio descriptions of mainstream movies. MAD contains over 384,000 natural language sentences grounded in over 1,200 hours of videos and exhibits a significant reduction in the currently diagnosed biases for video-language grounding datasets. MAD's collection strategy enables a novel and more challenging version of video-language grounding, where short temporal moments (typically seconds long) must be accurately grounded in diverse long-form videos that can last up to three hours. "
+    	],
 	    ['10754/669602', 2, 22, 9, '15ZWp1RW4Dw50noa0XzxPTFa0xN5KOx60', '1nvBpTdZb0uY-vcY5YV6PJEpIfMrkgcPu', 'SCTN: Sparse Convolution-Transformer Network for Scene Flow Estimation', [133, 135, 2, 0],
             [0, 0, 0, 0],
             [],
